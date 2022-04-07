c98309d5b56c219ec98f34233e91f1111d85b8de
shinyfuwante <mr.philliptang@gmail.com>
2022-04-07 00:34:03 -0700

    Introduce core features
    This commit introduces the ability to reset and then resize the grid (up to 100).
    A color picker was also implemented and the basic drawing functionality
    is implemented.

--- script.js
+++ script.js
@@ -1,16 +1,56 @@
-const SIZE = 32;
+let size = 16;
 const gridContainer = document.querySelector('grid_container');
-for (let i = 0; i < SIZE**2; i++) {
+let mouseDown = false;
+let color = 'black';
+
+function createDiv(size) {
     const pixel = document.createElement('div');
-
-    if (i%3 == 1) pixel.style.backgroundColor = 'yellow';
-    else if (i%2 == 1) pixel.style.backgroundColor = 'blue'; 
-    else pixel.style.backgroundColor = 'red';
-
-    pixel.style.width = `${100/SIZE}%`; 
-    pixel.style.height = 'auto';
-
-
-    gridContainer.appendChild(pixel);
+    pixel.style.width = `${100/size}%`; 
+    pixel.style.height = `${100/size}%`; 
+    pixel.classList.add('pixel');
+    return pixel;
 }
+
+function createPad(size) {
+    //create size**2 amount of divs to throw into a container
+    for (let i = 0; i < size**2; i++) {
+        gridContainer.appendChild(createDiv(size));
+    }
+}
+
+function draw(e) {
+    if (e.type == 'mouseover' && !mouseDown) return;
+    if (e.target.classList.contains("pixel")) e.target.style.backgroundColor = color;
+}
+
+function resetGrid(e) {
+    size = prompt('What size grid do you want? Max of 100.')
+    console.log(size);
+    clearGrid();
+    createPad(size);
+}
+
+function clearGrid() {
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+}
+
+function updateColor(e) {
+    color = e.target.value;
+}
+
+createPad(size);
+
+window.addEventListener('mousedown', () => mouseDown = true);
+window.addEventListener('mouseup', () => mouseDown = false);
+
+gridContainer.addEventListener('mouseover', draw);
+gridContainer.addEventListener('mousedown', draw);
+
+const resetButton = document.querySelector("button.reset");
+resetButton.addEventListener('click', resetGrid);
+
+const colorPicker = document.querySelector(".colorpicker");
+colorPicker.addEventListener('change', updateColor);
 
